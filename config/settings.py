6a3087d7ba71ebbf69e894089adcc735b21e01ed
IvanPyrlik <ivan.pyrlik59@gmail.com>
2024-03-11 18:42:00 +0500

    fix project

--- config/settings.py
+++ config/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'catalog',
         'USER': 'postgres',
         'HOST': 'localhost',
