951c8eacc65c6af1209ff3622303f068c89ccc48
IvanPyrlik <ivan.pyrlik59@gmail.com>
2024-03-14 17:40:40 +0500

    fix project

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts, product
 
-urlpatterns = [path('', home), path('contacts/', contacts, name='contacts')]
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('<int:pk>/product/', product, name='product'),
+]
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Магазин продуктов'
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'Ваше {name}, ваш телефон {phone}, ваше сообщение {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(product_id=pk),
+        'title': f'Вы выбрали: {product_item.name}'
+    }
+    return render(request, 'catalog/product.html', context)
