c9be6b4d99061f645175b5a9243e6faa1a43182c
IvanPyrlik <ivan.pyrlik59@gmail.com>
2024-03-15 08:11:35 +0500

    fix project

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,6 @@ from catalog.views import contacts, product, products
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    #path('', home, name='home'),
     path('', products, name='products'),
     path('contacts/', contacts, name='contacts'),
     path('<int:pk>/product/', product, name='product'),
